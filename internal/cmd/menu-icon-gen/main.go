3d5533a0ffa2fb794a0ff0c5d3b9a9a4ec8d3118
Boidushya <boidushyabhattacharya@gmail.com>
2026-06-09 19:00:33 +0530

    style: tune tray menu icon glyph to 32

--- internal/cmd/menu-icon-gen/main.go
+++ internal/cmd/menu-icon-gen/main.go
@@ -31,7 +31,7 @@ type icon struct {
 // glyph rendered into a centered region of that canvas so the icon looks
 // smaller in the menu without losing rasterization resolution.
 const canvasSize = 36
-const glyphSize = 34
+const glyphSize = 32
 
 func main() {
 	icons := []icon{
